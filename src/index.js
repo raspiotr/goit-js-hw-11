cab3ac59284957cd1140b392609fa2179df23f80
raspiotr <raspiotr@gmail.com>
2023-09-26 17:44:24 +0200

    create gallery

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ const searchBtn = document.querySelector('button[type="submit"]');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.loadmore');
 
+gallery.style.display = 'flex';
+gallery.style.alignItems = 'center';
+gallery.style.justifyContent = 'space-between';
+gallery.style.flexWrap = 'wrap';
+gallery.style.gap = '10px';
+
 let page = 1;
 
 const searchParams = new URLSearchParams({
@@ -22,18 +28,52 @@ const searchParams = new URLSearchParams({
   page,
 });
 
+const createImageCard = image => {
+  const card = document.createElement('div');
+  card.classList.add('photo-card');
+  card.innerHTML = `
+            <a href="${image.largeImageURL}" class="gallery-item" data-lightbox="image">
+              <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            </a>
+            <div class="info">
+              <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+              <p class="info-item"><b>Views:</b> ${image.views}</p>
+              <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+              <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+            </div>
+          `;
+  gallery.append(card);
+};
+
 const fetchImages = async () => {
   const response = await axios.get('https://pixabay.com/api/', {
     params: searchParams,
   });
   const data = response.data;
   console.log(data);
+
+  if (data.hits.length === 0) {
+    Notiflix.Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  if (page === 1) {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  }
+  data.hits.forEach(createImageCard);
 };
 
-searchForm.addEventListener('submit', event => {
+searchForm.addEventListener('submit', async event => {
   event.preventDefault();
   const searchQuery = searchInput.value;
   console.log(searchQuery);
-  searchParams.set('q', searchQuery);
-  fetchImages();
+
+  if (searchQuery) {
+    searchParams.set('q', searchQuery);
+    page = 1;
+    gallery.innerHTML = '';
+    fetchImages();
+  }
 });
